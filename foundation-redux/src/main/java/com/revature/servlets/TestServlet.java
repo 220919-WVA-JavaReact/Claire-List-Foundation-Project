cf6087065880ae3cbcef359016be1600645ca4cf
Claire List <claire.list2501@gmail.com>
2022-10-13 09:43:06 -0600

    servlet fun

--- foundation-redux/src/main/java/com/revature/servlets/TestServlet.java
+++ foundation-redux/src/main/java/com/revature/servlets/TestServlet.java
@@ -1,42 +1,15 @@
 package com.revature.servlets;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 public class TestServlet extends HttpServlet {
 
     @Override
-    public void init() throws ServletException {
-        System.out.println("[LOG] - SanityServlet Instantiated!");
-        System.out.println("[LOG] - Init param test-init-key: " + this.getServletConfig().getInitParameter("test-init-key"));
-        System.out.println("[LOG] - Context param test-init-key: " + this.getServletContext().getInitParameter("test-context-key"));
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("[LOG] - SanityServlet Destroyed");
-    }
-
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        // We'll just some println statements to check some information about the request
-        System.out.println("[LOG] - SanityServlet received a request at " + LocalDateTime.now());
-        System.out.println("[LOG] - Request URI: " + req.getRequestURI());
-        System.out.println("[LOG] - Request Method: " + req.getMethod());
-        System.out.println("[LOG] - Request Header, example: " + req.getHeader("example"));
-        System.out.println("[LOG] - Request Query String: " + req.getQueryString());
-
-        // We'll formulate a basic response to send back
-        // Set the status code
-        resp.setStatus(200); // It is 200 by default
-        resp.setHeader("Content-type", "text/plain");
-        resp.setHeader("example-response-header", "some-example-value");
-        resp.getWriter().write("This is the response payload");
+        resp.getWriter().write("<h1>/test works!</h1>");
     }
 
 }
---
+++ foundation-redux/src/main/java/com/revature/util/ContextLoaderListener.java
@@ -0,0 +1,43 @@
+package com.revature.util;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.revature.servlets.UserServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRegistration;
+import java.time.LocalDateTime;
+
+    public class ContextLoaderListener implements ServletContextListener {
+
+        @Override
+        public void contextInitialized(ServletContextEvent sce) {
+            System.out.println("[LOG] - The servlet context was initialized at " + LocalDateTime.now());
+
+            // We can also programmatically define/register our servlets with the container here
+
+            ObjectMapper mapper = new ObjectMapper();
+           // UserServlet userServlet = new UserServlet(mapper); //CREATE ME!
+
+            // Now I need to register it with the context itself
+            ServletContext context = sce.getServletContext();
+            ServletRegistration.Dynamic registeredServlet = context.addServlet("UserServlet", userServlet);
+            // Now I can affect the fields that I wanted to before
+            registeredServlet.addMapping("/users");
+            registeredServlet.setLoadOnStartup(3);
+            registeredServlet.setInitParameter("user-servlet-key", "user-servlet-value");
+            registeredServlet.setInitParameter("another-param", "another-value");
+
+        /*
+        ExampleServlet exampleServlet = new ExampleServlet(mapper);
+        context.addServlet("ExampleServlet", exampleServlet).addMapping("/examples");
+         */
+        }
+
+        @Override
+        public void contextDestroyed(ServletContextEvent sce) {
+            System.out.println("[LOG] - The servlet context was destroyed at " + LocalDateTime.now());
+        }
+}
